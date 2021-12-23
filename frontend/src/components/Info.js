ead37b8140a0d85f7ad78b83a74d8fa7b6f0d00d
nickel <damien.desouza1@gmail.com>
2021-12-23 13:09:39 +0100

    adding some components

---
+++ frontend/src/components/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Info.css';
+
+
+function Info() {
+
+  return (
+    <div className = "Info"><p>info échanges : </p></div>
+    
+  );
+}
+
+export default Info;
